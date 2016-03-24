d08227e6a0bfe53ec6b9c4e0990546cbbdaea372
pant2002 <pant.aniket2002123@gmail.com>
2016-03-23 22:11:25 -0500

    Coding LoadToScene script

---
+++ PhysicsBasketball/Assets/SciptsMainMenu/LoadOnClick.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadOnClick : MonoBehaviour {
+
+	public static void LoadScene(string sceneName, SceneManagement.LoadSceneMode mode = LoadSceneMode.Single);
+	{
+
+
+	}
+
+
+
+}
